5ed944e05ab02d16d4d8e1eeb6d005a976a73ba6
Rosinni Rodríguez <43686922+rosinni@users.noreply.github.com>
2025-08-08 09:22:02 +0000

    add validAuth function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,17 @@
+import React, {useEffect} from "react";
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import { getUserFavorites, validAuth } from "../services/userServices.js";
 
 export const Home = () => {
 
   const {store, dispatch} =useGlobalReducer()
+
+
+
+  useEffect(()=>{
+	getUserFavorites()
+  },[])
 
 	return (
 		<div className="text-center mt-5">
--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,33 +1,74 @@
 //funciones que tienen que ver con el sistema de autenticacion
 
-export async function login(email,password) {
+export async function login(email, password) {
     console.log("Soy un console log del servicio");
-    console.log(email,password);
-    
+    console.log(email, password);
+
 
 
     try {
-        const response = await fetch('https://turbo-winner-vq65p4559jghxp4w-3000.app.github.dev/login',{
+        const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/login`, {
             method: 'POST',
-            body: JSON.stringify({email:email,password:password}),
-            headers:{
-                'Content-Type':'application/json'
+            body: JSON.stringify({ email: email, password: password }),
+            headers: {
+                'Content-Type': 'application/json'
             }
         })
         const data = await response.json()
         console.log(data);
-        
+
         if (response.status === 200) {
-            localStorage.setItem('token',data.access_token)
+            localStorage.setItem('token', data.access_token)
             return true
         }
         if (response.status === 404) {
             return false
         }
-        
-        
+
+
     } catch (error) {
         console.log(error)
         return false
     }
+}
+
+
+export async function getUserFavorites() {
+
+    let token = localStorage.getItem("token")
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${token}`)
+
+    const requestOptions = {
+        method: "GET",
+        headers: myHeaders
+    };
+
+    try {
+        const response = await fetch(import.meta.env.VITE_BACKEND_URL + "/favorites", requestOptions);
+        const result = await response.json();
+        console.log(result)
+    } catch (error) {
+        console.error(error);
+    };
+}
+
+//declaracion de funcion para validar la autenticacion
+export async function validAuth() {
+    let token = localStorage.getItem("token")
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${token}`)
+
+    const requestOptions = {
+        method: "GET",
+        headers: myHeaders
+    };
+
+    try {
+        const response = await fetch(import.meta.env.VITE_BACKEND_URL +"/valid-auth", requestOptions);
+        const result = await response.json();
+        return result.logged
+    } catch (error) {
+        console.error(error);
+    };
 }
